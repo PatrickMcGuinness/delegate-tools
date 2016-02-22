6ba064a9742ecfb37ff7d2e9c00dfd54ec439635
PatrickMcGuinness <patmcguinness.phd@gmail.com>
2016-02-22 17:30:13 -0600

    Updated readpolldata.py, reads the polls from CSV and prints out formatted poll (another CSV) for delegatealloc.py. Also creates a JSON, which can be used as well.

--- readpolldata.py
+++ readpolldata.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+from __future__ import print_function
 import sys
 import csv
 statemap = { 'AL': 'Alabama', 'AK': 'Alaska', 'AR': 'Arkansas', 'GA': 'Georgia',
@@ -8,71 +9,62 @@ statemap = { 'AL': 'Alabama', 'AK': 'Alaska', 'AR': 'Arkansas', 'GA': 'Georgia',
              'TN': 'Tennessee', 'TX': 'Texas', 'VT': 'Vermont', 'VA': 'Virginia',
              'MN': 'Minnesota', 'WY':'Wyoming', 'MS': 'Mississippi', 'MO':'Missouri',
              'AZ': 'Arizona', 'UT':'Utah'}
-# poll has
-# Date
-# Area - State and CD
-#
 
-def main():
-    if (len(sys.argv) == 2):
-        inputfile = sys.argv[1]
-    elif (len(sys.argv) == 3):
-        option = sys.argv[2]
-        inputfile = sys.argv[2]
+def p2f(x):
+    if "%" in x:
+        return str(float(x.strip('%'))/100)
+    elif "," in x:
+        return str(int(x.replace(",",'')))
     else:
-        print("usage: <program> inputfile")
-        sys.exit()
-    pollrows = read_csv(inputfile)
-    read_pollrows(pollrows)
-    #CandPoll = read_polls_ivr(inputfile)
-    return
+        return x
 
 # Make CandPoll a dictionary of polls, for each state and CD
 # Texas, TexasCD1, etc.  TexasCD1: { "Carson":0.08, "Cruz":0.05} etc.
 # Date.
-def read_csv(inputfile):
+def read_ivr_csv(inputfile):
     with open(inputfile, 'rb') as csvfile:
         pollrows = csv.reader(csvfile, delimiter=',', quotechar='"')
         polldata=[]
         for row in pollrows:
             polldata.append(row)
-            #print row
     return polldata
 
-def read_polls_ivr(inputfile):
-    with open(inputfile) as f:
-        inputlines = [x.strip('\n') for x in f.readlines()]
-        CandPoll = {}
-        headerinfo = []
-        statename ='none'
-
 def read_pollrows(inputlines):
+    CandPoll={}
     for linenum, rowdata in enumerate(inputlines):
         if rowdata[0]=='Date':
             headerinfo=rowdata
-            print "Location",
-            for i in headerinfo[15:24]:
-                print ",",i,
-            print
-            #for i,ind in enumerate(headerinfo):
-                #print i,":",ind,
+            print("Location,",end=" ")
+            print(", ".join(headerinfo[15:24]))
+            pollkey = headerinfo[15:24]
         else:
-            polldata = rowdata
-            if 'CD' in polldata[1]:
-                pollarea = statename+polldata[1].strip()
+            if 'CD' in rowdata[1]:
+                pollarea = statename+rowdata[1].strip()
                 #print pollarea, statename, statemap[statename], polldata[1].strip()
             else:
                 #print 'state match', polldata[1]
-                statename = polldata[1]
-                pollarea = polldata[1]
+                pollarea = statename = rowdata[1]
             if statename in statemap:
-                print pollarea,
-                for i in polldata[15:24]:
-                    print ",",i,
-                print
-	    #print rowdata
-    return
+                polldata = list(map(p2f, rowdata[15:24]))
+                if polldata[5] !='':
+                    print(pollarea,end="")
+                    print(",",", ".join(polldata))
+                CandPoll[pollarea] = {pollkey[i]:polldata[i] for i in xrange(len(pollkey))}
+    return CandPoll
 
+def main():
+    if (len(sys.argv) == 2):
+        inputfile = sys.argv[1]
+    elif (len(sys.argv) == 3):
+        option = sys.argv[1]
+        inputfile = sys.argv[2]
+    else:
+        print("usage: <program> <option> inputfile")
+        sys.exit()
+    if option and option == 'ivr':
+        pollrows = read_ivr_csv(inputfile)
+        CandPoll = read_pollrows(pollrows)
+    return
 
 if __name__ == '__main__':
     main()
